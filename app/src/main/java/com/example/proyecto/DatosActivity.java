f0b29582c3d79a919bee6d81525cfc52f3fe57b7
Kenneth Lopez <lopezken@hotmail.com>
2020-10-29 13:50:50 -0600

    Agregamos un chingo de cosas

---
+++ app/src/main/java/com/example/proyecto/DatosActivity.java
@@ -0,0 +1,28 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+public class DatosActivity extends AppCompatActivity {
+    private TextView txtNombre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos);
+        txtNombre = findViewById(R.id.txt_nombre);
+        NumberPicker npedad= findViewById(R.id.npEdad);
+        npedad.setMinValue(0);
+        npedad.setMaxValue(100);
+
+    }
+    public void siguiente(View view){
+        Intent i = new Intent(this, Menu.class);
+        startActivity(i);
+    }
+}
--- app/src/main/java/com/example/proyecto/Inicio.java
+++ app/src/main/java/com/example/proyecto/Inicio.java
@@ -2,7 +2,9 @@ package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Inicio extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class Inicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
     }
+    public void siguiente(View view) {
+        Intent i = new Intent(this, DatosActivity.class);
+        startActivity(i);
+    }
+
 }
